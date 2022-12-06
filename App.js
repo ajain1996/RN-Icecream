dec95aa724673afe01f44943e6d6f0d8c5963dc5
lakheraakshay <akshay.lakhera@easystepin.com>
2022-12-06 22:59:18 +0530

    splash

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
-import { COLORS } from './src/component/Constant/Color';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {COLORS} from './src/component/Constant/Color';
+import {useDispatch, useSelector} from 'react-redux';
 import Auth from './src/service/Auth';
-import { setUser } from './src/redux/reducer/user';
+import {setUser} from './src/redux/reducer/user';
 import NavigationStack from './src/navigation/NavigationStack';
 import AuthenticationStack from './src/navigation/AuthenticationStack';
 import Navigation from './src/service/Navigation';
-import { Alert } from 'react-native';
-import { getUserById } from './src/utils/API';
+import {Alert} from 'react-native';
+import {getUserById} from './src/utils/API';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const dispatch = useDispatch();
 
-  const { userData, login } = useSelector(state => state.User);
+  const {userData, login} = useSelector(state => state.User);
   // const [login, setLogin] = useState(false);
 
   const [loginChk, setLoginChk] = useState(true);
@@ -29,7 +29,7 @@ export default function App() {
     let data = await Auth.getAccount();
     console.log('data fetched: ', data);
     console.log('User fetched: ', data);
-    Alert.alert('splash');
+    // Alert.alert('splash');
     // console.log('\n\n\n\n\n\n\n\n', data, '<<<<\n\n\n\n\n\n this is data');
     // return null;
     if (data !== null) {
@@ -66,7 +66,7 @@ export default function App() {
         detachInactiveScreens={false}
         initialRouteName="Auth"
         screenOptions={{
-          cardStyle: { backgroundColor: COLORS.white },
+          cardStyle: {backgroundColor: COLORS.white},
           gestureEnabled: true,
           backgroundColor: COLORS.button,
           gestureDirection: 'horizontal',
